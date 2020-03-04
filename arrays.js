ccfd91e5dc73a0ee65c3838781483f2308d0f928
George Lugo <stg77713@gmail.com>
2020-03-04 02:24:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(ele, arr)
+function addElementToBeginningOfArray(arr, ele) {
+  return [element, ...array]
+}
 
-function destructivelyAddElementToBeginningOfArray(ele, arr)
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+  array.unshift(element)
+  return array 
+}
